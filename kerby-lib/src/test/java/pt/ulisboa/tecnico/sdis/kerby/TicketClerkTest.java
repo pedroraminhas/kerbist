ac5248c98074af8433117e4b5d99a221ab94e39e
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2018-03-09 11:06:17 +0000

    Create Ticket class to encapsulate TicketView and replace Ticket

--- kerby-lib/src/test/java/pt/ulisboa/tecnico/sdis/kerby/TicketClerkTest.java
+++ kerby-lib/src/test/java/pt/ulisboa/tecnico/sdis/kerby/TicketClerkTest.java
@@ -196,7 +196,7 @@ public class TicketClerkTest {
 		// seal ticket with server key
 		final Key serverKey = generateKey();
 		SealedView sealedTicket = clerk.ticketSeal(ticket, serverKey);
-		
+
 		// decipher ticket with server key
 		TicketView decipheredTicket = clerk.ticketUnseal(sealedTicket, serverKey);
 
